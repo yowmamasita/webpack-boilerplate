835ea8d4b035069e38d743afb3448091783b2b8f
Ben Sarmiento <ben.sarmiento@cloudsherpas.com>
2015-10-15 03:34:55 +0800

    Start the day with :coffee:

---
+++ README.md
@@ -0,0 +1,17 @@
+# Webpack + Babel Boilerplate
+
+## Dependencies
+
+`npm install rimraf webpack eslint -g`
+
+## Usage
+
+Inside the repo folder, do `git archive vanilla | tar -x -C project_path`
+
+And then cd to `project_path` and do
+
+`npm install babel-core babel-eslint babel-loader html-loader css-loader url-loader eslint express rimraf webpack webpack-dev-middleware webpack-hot-middleware --save-dev`
+
+Development `npm start`
+
+Production `npm run build`
---
+++ webpack.config.dev.js
@@ -0,0 +1,38 @@
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+  devtool: 'eval',
+  entry: [
+    'webpack-hot-middleware/client',
+    './src/scripts/index'
+  ],
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'bundle.js',
+    publicPath: '/static/'
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoErrorsPlugin()
+  ],
+  module: {
+    loaders: [{
+      test: /\.js$/,
+      loaders: ['babel'],
+      include: path.join(__dirname, 'src')
+    }, {
+      test: /\.html$/,
+      loaders: ['html-loader'],
+      include: path.join(__dirname, 'src')
+    }, {
+      test: /\.css$/,
+      loaders: ['css-loader'],
+      include: path.join(__dirname, 'src')
+    }, {
+      test: /\.(png|woff|woff2|eot|ttf|svg)$/,
+      loaders: ['url-loader'],
+      include: path.join(__dirname, 'src')
+    }]
+  }
+};
